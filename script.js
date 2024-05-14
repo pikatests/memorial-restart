16839e74cd6f5abb1bf0876bf8c71dab97ca1d13
Alejandra Thomas <alejandralupio.identifix@gmail.com>
2024-05-14 19:15:59 -0400

    fix text

--- script.js
+++ script.js
@@ -34,13 +34,13 @@ const ruLang = [
     './assets/desktop/retirementf_ru.png',
     './assets/desktop/goldenweddingf_ru.png',
     './assets/desktop/twentynineteen.png',
-    './assets/navbook/milestones_ru.webp',
-    './assets/navbook/stories_ru.webp',
-    './assets/navbook/gallery_ru.webp',
-    './assets/navbook/my_granny_ru.webp',
-    './assets/navbook/remember_ru.webp',
-    './assets/navbook/map_ru.webp',
-    './assets/navbook/nanas_words_ru.webp',
+    './assets/navbook/milestones_en.webp',
+    './assets/navbook/stories_en.webp',
+    './assets/navbook/gallery_en.webp',
+    './assets/navbook/my_granny_en.webp',
+    './assets/navbook/remember_en.webp',
+    './assets/navbook/map_en.webp',
+    './assets/navbook/nanas_words_en.webp',
   ]
   
   
@@ -125,20 +125,64 @@ const ruLang = [
     './assets/tablet/retirement_en.png',
     './assets/tablet/goldenwedding_en.png',
     './assets/tablet/twentynineteen.png',
-    './assets/navbook/milestones_ru.webp',
-    './assets/navbook/stories_ru.webp',
-    './assets/navbook/gallery_ru.webp',
-    './assets/navbook/my_granny_ru.webp',
-    './assets/navbook/remember_ru.webp',
-    './assets/navbook/map_ru.webp',
-    './assets/navbook/nanas_words_ru.webp',
+    './assets/navbook/milestones_en.webp',
+    './assets/navbook/stories_en.webp',
+    './assets/navbook/gallery_en.webp',
+    './assets/navbook/my_granny_en.webp',
+    './assets/navbook/remember_en.webp',
+    './assets/navbook/map_en.webp',
+    './assets/navbook/nanas_words_en.webp',
+  ]
+
+  const enText = [
+    'Born in the village of Vlasovo, Moscow region in a family of poor peasants Tatyana Vasilievna and Yegor Fedorovich.<br><br> The father died in the war and the mother was left with four children in her arms, the youngest of whom was only one month old.',
+    'Baptism',
+    'She was baptised in the Vlasov Church of the Intercession of the Most Holy Theotokos.<br> The church was of great importance not only in the life of Olga Yegorovna, but also in her parents, grandmother and all previous generations of the family.',
+    'School<br> years',
+    'Work at the Chemical Plant in Roshal and study at the Roshal Chemical-Technological College in the evening department.<br>Education has always been the main thing for her and she was in no hurry to get married until she graduated from college. ',
+    'She married a fellow villager Anatoly Sergeevich Shmelev.<br> After getting married, they moved to Moscow and got a job there.',
+    'Birth of daughters',
+    'Tatiana was born in 1961, Elena in 1967.<br> Elena was born with a pathology and Olga Yegorovna had to temporarily leave her job to take care of the sick child.<br> Despite all the difficult circumstances, there was not even a thought to abandon the girl.<br>In 1984, Elena died.',
+    'Work in the state secret Research Institute of Chemistry and Mechanics',
+    '',
+    'Birth of a<br> beloved<br> grandson',
+    'Retirement',
+    'Olga Yegorovna and Anatoly Sergeevich spent their retirement years in half in Moscow and in their native village of Vlasovo.<br> They were engaged in farming and raising a grandson who came every summer.',
+    'Golden<br> wedding',
+    'After a long illness, she died at home surrounded by her close ones.',
+  ]
+
+  const ruText = [
+    'Родилась в селе Власово, Московской области в семье простых крестьян Татьяны Васильевны и Егора Фёдоровича. <br><br> Отец погиб на войне и мать осталась с четырьмя детьми на руках, младшему из которых был всего один месяц.',
+    'Крещение',
+    'Была крещена во Власовской церкви Покрова Пресвятой Богородицы.<br> Храм имел большое значение не только в жизни Ольги Егоровны, но и её родителей, бабушки и всех предыдущих поколений семьи.',
+    'Школьные',
+    'Работа на Химзаводе в Рошале и учёба в Рошальском Химико-Технологическом техникуме на вечернем отделении. <br><br> Образование всегда было главным для бабушки, и она не спешила замуж до окончания техникума. ',
+    'Вышла замуж <br> за односельчанина Шмелёва Анатолия Сергеевича. <br> После женитьбы переехали в Москву и устроились на работу.',
+    'ождение<br> дочерей',
+    'Татьяна 1961-го и Елена 1967-го годов рождения. <br><br> Леночка родилась нездоровой и Ольге Егоровне пришлось временно уйти с работы, чтобы ухаживать за больным ребёнком.<br><br> Несмотря на все тяжёлые обстоятельства, не было даже мысли отказаться от девочки.<br><br> В 1984 году Леночки не стало.',
+    'Работа <br> в секретном НИИХМ',
+    '(Научно-исследовательский институт химии и механики) ',
+    'Рождение<br> любимого<br> внука',
+    'Выход<br> на пенсию',
+    'Свои пенсионные годы Ольга Егоровна и Анатолий Сергеевич проводили в Москве и в родном селе Власово. <br><br> Они занимались сельским хозяйством и воспитанием внука, который приезжал к ним каждое лето.',
+    'Золотая<br> свадьба',
+    'После продолжительной болезни умерла дома в кругу родных.',
   ]
   
   function changeLanguage(language) {
     const imageElements = document.querySelectorAll('.changeable-img')
+    const textElements = document.querySelectorAll('.changeable-txt')
   
     const isMobile = window.innerWidth < 800
     const isTablet = window.innerWidth >= 800 && window.innerWidth < 1366
+
+    const currentText = 
+      language === 'russian' ? ruText : enText
+
+    textElements.forEach((text, index) => {
+      text.innerHTML = currentText[index]
+    })
   
     const currentImages =
       language === 'russian'
